ecb2dbddb7946e6828afe4976bd794d564c929d6
Sai Tejas A R <46528244+Saitejas21@users.noreply.github.com>
2021-08-22 15:40:57 +0530

    added: app.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Welcome to Emoji Docs</h1>
+      <h2>Search your emoji and know about it.</h2>
     </div>
   );
 }
